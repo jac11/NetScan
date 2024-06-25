4a2dee03fd75867e9978143baee6ed843c49aafd
jac11 <jac11devel@gmail.com>
2024-06-25 18:43:44 -0400

    Add files via upload

--- ScanNet.go
+++ ScanNet.go
@@ -7,6 +7,7 @@ import (
     "flag"
     "time"
     "strconv"
+    "sync"
 )
 
 
@@ -25,22 +26,32 @@ func ScanSinglPort(Domain string,Port string){
        fmt.Println("Connection Fail in Port ", Port)
        return
    }
-   fmt.Println("Connection Suessed Working On Port" ,Port)
+   fmt.Println("Connection Succeeded Working On Port %d\n", Port)
    Connect.Close()
 }
 func ScanRangePort(Domain string,Start string , End string){
+	var WaitGroup sync.WaitGroup
+	var mutex sync.Mutex
 	StartInt,_ := strconv.Atoi(Start)
 	EndInt,_ := strconv.Atoi(End)
     for Port := StartInt ; Port <= EndInt ;Port++{
-    	DomainNet := net.JoinHostPort(Domain,strconv.Itoa(Port))
-        _, err := net.DialTimeout("tcp",DomainNet,3*time.Second)
-        if err != nil{
-           fmt.Println("Connection Fail in Port ", Port)
-
-        }else{
-        	 fmt.Println("Connection Suessed Working On Port" ,Port)
-        }
-    }    
+        WaitGroup.Add(1)
+        go func (Port int){
+        	defer  WaitGroup.Done()
+	    	DomainNet := net.JoinHostPort(Domain,strconv.Itoa(Port))
+	        _, err := net.DialTimeout("tcp",DomainNet,3*time.Second)
+	        mutex.Lock()
+			defer mutex.Unlock()
+	        if err != nil{
+	           fmt.Printf("\rConnection Fail in Port %d", Port)
+	           time.Sleep(10 * time.Millisecond)
+	        }else{
+	        	fmt.Printf("\rConnection Succeeded Working On Port %d\n", Port)
+	        }
+        }(Port)
+    }
+    WaitGroup.Wait() 
+    fmt.Println()  
 }
 func main(){
     var DataInfo Config
@@ -50,7 +61,7 @@ func main(){
 	flag.StringVar(&DataInfo.EndScan,"EndScan","","End Of Port Sacn")
 	flag.Parse()
 	if DataInfo.Domain == ""{
-		fmt.Println("Domain name or ip Not Valed")
+		fmt.Println("Domain name or IP Not Valid")
 		return
 	}
 	if DataInfo.StartScan != "" && DataInfo.EndScan !=""{
