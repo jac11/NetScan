6160d07154a8e9daf8eafc728c69079faacf79c4
jac11 <jac11devel@gmail.com>
2024-06-27 03:46:26 -0400

    Add files via upload

--- ScanNet.go
+++ ScanNet.go
@@ -8,7 +8,18 @@ import (
     "time"
     "strconv"
     "sync"
+    "strings"
 )
+
+var Reset = "\033[0m" 
+//var Red = "\033[31m" 
+//var Green = "\033[32m" 
+//var Yellow = "\033[33m" 
+var Blue = "\033[34m" 
+//var Magenta = "\033[35m" 
+//var Cyan = "\033[36m" 
+//var Gray = "\033[37m" 
+//var White = "\033[97m"
 
 
 type Config struct{
@@ -26,7 +37,7 @@ func ScanSinglPort(Domain string,Port string){
        fmt.Println("Connection Fail in Port ", Port)
        return
    }
-   fmt.Println("Connection Succeeded Working On Port %d\n", Port)
+   fmt.Println("Connection Succeeded Working On Port ", Port)
    Connect.Close()
 }
 func ScanRangePort(Domain string,Start string , End string){
@@ -53,13 +64,42 @@ func ScanRangePort(Domain string,Start string , End string){
     WaitGroup.Wait() 
     fmt.Println()  
 }
+func Style (Port  string ,Domain string,Start string , End string) {
+    var Banner string = `
+███████  ██████  █████  ███    ██ ███    ██ ███████ ████████ 
+██      ██      ██   ██ ████   ██ ████   ██ ██         ██    
+███████ ██      ███████ ██ ██  ██ ██ ██  ██ █████      ██    
+     ██ ██      ██   ██ ██  ██ ██ ██  ██ ██ ██         ██    
+███████  ██████ ██   ██ ██   ████ ██   ████ ███████    ██    
+                        @jacstory`+"\n"
+                        
+    fmt.Println(Blue+Banner+Reset)  
+    if Port !="" && Domain !="" &&End == "" && Start==""{
+       fmt.Println("Staring Port     -----------| > ",Port)
+       fmt.Println("Staring Domain   -----------| > ",Domain)
+       fmt.Println(strings.Repeat("_", 40))
+    }else if Start !="" && End !=""{
+       fmt.Println("Staring Port     -----------| > ",Port)
+       fmt.Println("Staring Domain   -----------| > ",Domain)
+       fmt.Println("Staring Port     -----------| > ", Start)
+       fmt.Println("Ending  Port     -----------| > ", End)
+       fmt.Println("")
+       fmt.Println(strings.Repeat("_", 40))
+}
+    }                 
+    
+    
 func main(){
+    
     var DataInfo Config
+
     flag.StringVar(&DataInfo.Port,"Port","80","default Port Scan")
 	flag.StringVar(&DataInfo.Domain,"Domain","","IP/Domain To Scan")
 	flag.StringVar(&DataInfo.StartScan,"StartScan","","Start Range Of Port Scan")
 	flag.StringVar(&DataInfo.EndScan,"EndScan","","End Of Port Sacn")
 	flag.Parse()
+    Style(DataInfo.Port ,DataInfo.Domain, DataInfo.StartScan,DataInfo.EndScan )
+    
 	if DataInfo.Domain == ""{
 		fmt.Println("Domain name or IP Not Valid")
 		return
@@ -70,3 +110,5 @@ func main(){
     	ScanSinglPort(DataInfo.Domain,DataInfo.Port)
     }
 }
+
+
